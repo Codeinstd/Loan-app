d265e67c5b0c8625b5f542aa323788033f79a6af
David Oladipupo <52843114+Codeinstd@users.noreply.github.com>
2023-12-04 23:14:53 +0100

    bug fixed

--- lib/ui/screens/first.dart
+++ lib/ui/screens/first.dart
@@ -82,6 +82,218 @@ class _FirstState extends State<First> {
       ),
 
       // bottombar section //
+      bottomNavigationBar: BottomAppBar(
+        height: 55,
+        color: AppColors.white,
+        // elevation: 1,
+        padding: const EdgeInsets.only(left: 16, top: 10, right: 16, bottom: 0),
+        child: SizedBox(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              // home //
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      // Navigator.of(context).push(
+                      //   MaterialPageRoute(builder: (context) => const home()),
+                      // );
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/home.png',
+                          color: AppColors.iconcolor,
+                          height: 24,
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        // SizeConfig().p12medb('Home', 14),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              // payment //
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      // Navigator.of(context).push(
+                      //   MaterialPageRoute(
+                      //       builder: (context) => const payment()),
+                      // );
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Image.asset(
+                          'assets/images/payment.png',
+                          color: AppColors.iconcolor,
+                          height: 24,
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        // SizeConfig().p12medb('Payment', 14),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              // QR code //
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      showModalBottomSheet(
+                          backgroundColor: AppColors.white,
+                          isDismissible: true,
+                          isScrollControlled: true,
+                          enableDrag: true,
+                          context: context,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.vertical(top: Radius.circular(20)),
+                          ),
+                          builder: (BuildContext context) {
+                            return SizedBox(
+                              width: 375,
+                              child: DecoratedBox(
+                                decoration: const BoxDecoration(
+                                  color: AppColors.white,
+                                  borderRadius: BorderRadius.vertical(
+                                    top: Radius.circular(20),
+                                  ),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(24),
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Row(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.spaceBetween,
+                                          children: [
+                                            // SizeConfig().p18medB('QR Code', 18),
+                                            // link //
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.pop(context);
+                                              },
+                                              child: const Icon(
+                                                Icons.close_sharp,
+                                                color: AppColors.iconcolor,
+                                              ),
+                                            ),
+                                          ]),
+                                      Image.asset(
+                                        'assets/images/QR_code.png',
+                                        height: 200,
+                                      ),
+                                      // SizeConfig()
+                                      //     .p12regB('Personal Unique Code', 12),
+                                      const SizedBox(
+                                        height: 20,
+                                      ),
+                                      // SizeConfig().p20medB('Coming Soon!', 20),
+                                      const SizedBox(
+                                        height: 16,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            );
+                          });
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        // Image.asset(
+                        //   'assets/images/qr.png',
+                        //   // color: AppColors.lightGrey,
+                        //   height: 24,
+                        // ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        // SizeConfig().p12medb('QR Code', 14),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              // chatroom //
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      // Navigator.of(context).push(
+                      //   MaterialPageRoute(
+                      //       builder: (context) => const chatroom()),
+                      // );
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        // Image.asset(
+                        //   'assets/images/chatroom.png',
+                        //   color: AppColors.lightGrey,
+                        //   height: 24,
+                        // ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        // SizeConfig().p12medb('Chatroom', 14),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+
+              // profile //
+
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        // Image.asset(
+                        //   'assets/images/user..png',
+                        //   color: AppColors.main,
+                        //   height: 24,
+                        // ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        // SizeConfig().p12medC('Profile', 14),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
 
       // body section //
       body: Column(),
